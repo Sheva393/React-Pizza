cfcf39ba83815f29b4459b9169acf8f3adb3c5fa
unknown <Sheva961gmail.com>
2020-12-28 03:02:39 +0300

    updateCart

--- src/redux/actions/pizzas.js
+++ src/redux/actions/pizzas.js
@@ -7,13 +7,13 @@ export const setLoaded = payload => ({
 
 
 
-export const fetchPizzas = ( sortBy , category) => (dispatch) => {
+export const fetchPizzas = (sortBy, category) => (dispatch) => {
     dispatch(setLoaded(false))
-    
-    axios.get(`http://localhost:3001/pizzas?${ category !==null ? `category=${category}` : ''}&_sort=${sortBy.type}&_order=${sortBy.order}`)
-    .then( ({data}) =>{ 
-    dispatch(setPizzas(data));
-    });
+
+    axios.get(`/pizzas?${category !== null ? `category=${category}` : ''}&_sort=${sortBy.type}&_order=${sortBy.order}`)
+        .then(({ data }) => {
+            dispatch(setPizzas(data));
+        });
 
 };
 
@@ -21,7 +21,7 @@ export const fetchPizzas = ( sortBy , category) => (dispatch) => {
 
 export const setPizzas = (items) => ({
     type: 'SET_PIZZAS',
-    payload: items ,
+    payload: items,
 
 })
 
